95e031c64ef4d675ffb164a8090b7dd72b05ca00
sids278 <94206506+sids278@users.noreply.github.com>
2023-06-27 05:09:49 +0530

    Update math.test.js

--- math.test.js
+++ math.test.js
@@ -1,28 +1,13 @@
-// math.test.js
-const assert = require('assert');
 const { add, subtract } = require('./math');
 
-// Test case for the add function
-const testAdd = () => {
-    const result = add(2, 3);
-    assert.strictEqual(result, 5, 'Addition test failed');
-};
+describe('Math Functions', () => {
+    test('should add two numbers correctly', () => {
+        const result = add(2, 3);
+        expect(result).toBe(5);
+    });
 
-// Test case for the subtract function
-const testSubtract = () => {
-    const result = subtract(5, 3);
-    assert.strictEqual(result, 2, 'Subtraction test failed');
-};
-
-// Run the tests
-const runTests = () => {
-    try {
-        testAdd();
-        testSubtract();
-        console.log('All tests passed successfully');
-    } catch (error) {
-        console.error('Test failed:', error.message);
-    }
-};
-
-runTests();
+    test('should subtract two numbers correctly', () => {
+        const result = subtract(5, 3);
+        expect(result).toBe(2);
+    });
+});
